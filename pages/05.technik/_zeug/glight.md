9f28c8ec2c53ecc40eb239c459f963a5b58cd6be
Jan Martens <jan.niklas.martens@mni.thm.de>
2024-08-01 16:36:23 +0200

    Divide the css files and make own for carousel and gallery

--- pages/05.technik/_zeug/glight.md
+++ pages/05.technik/_zeug/glight.md
@@ -8,3 +8,6 @@ entryOuterPic: outerImage
 entryInnerPic: innerImage
 ---
 
+# Eine Übersicht unser Gerätschaft
+
+## Fahrzeuge
--- themes/feuerwehr-hungen/js/site.js
+++ themes/feuerwehr-hungen/js/site.js
@@ -69,6 +69,7 @@ jQuery(document).ready(function($){
 
     const menuButton = document.getElementById('toggle');
     const menuList = document.getElementById('tree-menu');
+    console.log(isTouch);
     if (isTouch){
         document.addEventListener('click', (event) => {
             if (!menuList.contains(event.target) && !menuButton.contains(event.target)) {
