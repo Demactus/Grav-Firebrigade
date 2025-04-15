2c0f599e13fb6812781c711ddc23ca5b053e62eb
Jan Martens <jan.niklas.martens@mni.thm.de>
2025-04-15 16:57:16 +0200

    Update products flexobjects for category usage

---
+++ plugins/conditional-fields/CHANGELOG.md
@@ -0,0 +1,11 @@
+# v1.0.1
+##  02/03/2024
+
+1. [](#bugfix)
+    * Fixed custom admin route
+
+# v1.0.0
+##  01/01/2021
+
+1. [](#new)
+    * ChangeLog started...
---
+++ plugins/conditional-fields/app/admin.js
@@ -0,0 +1,10 @@
+/**
+ * @author Greenrivers Team
+ * @copyright Copyright (c) 2022 Greenrivers
+ */
+
+import conditionalFields from './modules/conditional-fields';
+
+document.addEventListener("DOMContentLoaded", event => {
+    conditionalFields();
+});
